8e3e1e69fdcf4b2bb196144ea1445b90f445f0c3
jiimms <jiimms@yahoo.com>
2016-03-11 22:31:07 +0530

    first commit

---
+++ ruby4.4_quiz.rb
@@ -0,0 +1,27 @@
+Quiz
+
+	Take the Quiz
+		1)Which is true according to Modules?
+			a) Cannot be inherited (correct)
+			b) Same as class
+			c) Both
+
+		2) How will you reverse a string?
+			a) Using sort method
+			b) Using Slice method
+			c) Using reverse method (correct)
+
+		3) Integer can be divided into
+			a) FixNum and BigNum (correct)
+			b) Flaot and Decimal
+			c) Integer and Numeric
+
+		4) What is the output of this code? 5.class
+			a) Bignum
+			b) Fixnum (correct)
+			c) Integer
+
+		5) How you can define an array
+			a) Array.new
+			b) arr = [ ]
+			c) Both (correct)
